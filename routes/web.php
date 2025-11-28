c2531ce3556ecb1f82c8179b78f374251ee1c424
umair832 <umair.pcsvision@gmail.com>
2025-11-29 04:01:46 +0800

    Reasearching about Route (GET)

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,27 @@
 
 use Illuminate\Support\Facades\Route;
 
+//GET
 Route::get('/', function () {
-    return view('welcome');
+    return 'Main Page';
+});
+
+Route::get('/xxxx', function () {
+    return 'Hello World';
+})->name('hello');
+
+Route::get('/hallo', function () {
+    return redirect()->route('hello');
+});
+
+Route::get('/greet/{name}', function ($name) {
+    return 'Hello ' . $name . ' Welcome to our website';
+});
+
+//POST
+//PUT
+//DELETE
+
+Route::fallback(function () {
+    return 'Page Not Found';
 });
